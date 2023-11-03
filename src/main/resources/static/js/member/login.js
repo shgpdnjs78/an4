a5b146dc92c47d45bae7f99a8a67307b981d37db
an00 <shgpdnjs78@gmail.com>
2023-11-03 22:49:07 +0900

    AJEX

--- src/main/resources/static/js/member/login.js
+++ src/main/resources/static/js/member/login.js
@@ -9,48 +9,30 @@ function validateForm() {
         alert("아이디 또는 비밀번호를 입력해주세요.");
         return false;
     }
-    //로컬스토리지 정보 가져오기
-    //가져와서 펼치기(저장한다, 임시변수에)
-    //방금 입력한 유저 네임과 내가 펼친 자료의 아이디들을 비교해서 같은 것이 있는지
-    //확인 있다면 return true 없다면 false 하면서 아이디가 틀렸습니다.
-    // if (email = !user.e_mail) {
-    //     alert("입력하신 아이디는 회원정보에 없습니다.");
-    // }
-    // if (password = !user.password) {
-    //     alert("입력하신 아이디는 회원정보에 없습니다.");
-    // }
-    // return true
-    // var username = document.getElementById("email").value;
-    // var password = document.getElementById("password").value;
 
-    if (username === "" || password === "") {
-        alert("아이디 또는 비밀번호를 입력해주세요.");
-        return false;
-    }
-
-    var localStorageData = localStorage.getItem('USER');
-    if (localStorageData === null) {
-        alert("존재하지 않는 아이디입니다.");
-        return false;
-    }
-    var newData = JSON.parse(localStorageData);
-
-    for (var i = 0; i < newData.length; i++) {
-        if (username == newData[i].id) {
-            if (password == newData[i].password) {
-                alert("로그인 완료!");
-                return true;
-            }
-            else{
-                alert("비밀번호 틀렸다자식아")
-                return  false;
-            }
+    // 요청 메서드와 URL 설정
+    var url = "/user/login";
+    xhr.open("POST", url, true);
+    // 요청 헤더 설정 (만약 필요하다면)
+    xhr.setRequestHeader("Content-Type", "application/json");
+    // 요청이 완료되었을 때의 콜백 함수 설정
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // 요청이 성공적으로 완료됐을 때의 동작
+            console.log(xhr.responseText);
         }
-    }
-    alert("존재하지 않는 아이디입니다.");
-    return false;
+    };
+    // 보낼 데이터 생성 (만약 데이터를 보내야 한다면)
+    // 데이터를 JSON 문자열로 변환
+    var jsonData = JSON.stringify(
+        {
+            id : username,
+            pass : password
+        }
+    );
+    // 요청 전송
+    xhr.send(jsonData);
 
-}
 function validateWithdrawal() {
     var username = document.getElementById("logname").value;
     var email = document.getElementById("logemail").value;
